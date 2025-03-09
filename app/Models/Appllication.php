872b01ca32496af64e1e57898f9a3bf213527189
George-Ashraf <georgeid20@gmail.com>
2025-03-10 00:21:46 +0200

    first commit

---
+++ app/Models/Appllication.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Appllication extends Model
+{
+    /** @use HasFactory<\Database\Factories\AppllicationFactory> */
+    use HasFactory;
+}
---
+++ database/migrations/2025_03_09_214816_create_posts_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->text('skills')->nullable();
+            $table->string('comapny')->nullable();
+            $table->decimal('salary', 10)->nullable();
+            $table->string('job_title')->nullable();
+            $table->string('location')->nullable();
+            $table->enum('work_type', ['remote', 'hybrid', 'onsite'])->nullable();
+            $table->text('description')->nullable();
+            $table->enum('status', ['pending', 'published'])->nullable();
+            $table->string('image')->nullable();
+            $table->text('responsibility')->nullable();
+            $table->text('qualification')->nullable();
+            $table->text('benefits')->nullable();
+            $table->enum('experience_level', ['junior', 'mid_level', 'senior'])->nullable();
+            $table->date('closed_date')->nullable();
+            $table->integer('category_id')->nullable()->index('category_id');
+            $table->integer('user_id')->nullable()->index('user_id');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrentOnUpdate()->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
